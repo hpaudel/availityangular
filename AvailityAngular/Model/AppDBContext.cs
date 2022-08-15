c7a5145565a9c7dd9254990b400b9b675f7d0e9a
hpaudel <h2paudel@gmail.com>
2022-08-15 16:24:38 -0400

    Adding domain class

---
+++ AvailityAngular/Model/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace AvailityAngular.Model
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext( DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ AvailityAngular/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvailityAngular.Model
+{
+    public class User
+    {
+        public string FirstName     { get; set; }
+        public string LastName  { get; set; }
+        public string NPINumber { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public int Email { get; set; }
+
+    }
+}
